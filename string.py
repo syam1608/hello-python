3d17f94adb3679c443832bee1542a1bdf9641352
irham m s <irhammuhammadsyam@gmail.com>
2024-07-16 18:52:44 +0700

    menambah

---
+++ string.py
@@ -0,0 +1,27 @@
+#string
+data = "ini adalah string"
+print (data)
+print (type(data))
+#single quote (')
+data = 'ini single quote'
+print (data)
+#double q ("")
+#tanda \
+#backslash
+print ("C:\\user\\irham")
+#tab semakin jauh
+print ("aku\t\t\t semakin jauh")
+#backspace deketan
+print ("aku \bdeketan")
+#newline (\n  atau \r)
+#\n :>>> LF line feed
+#\r :>>> CR carriage return
+#\r\n :>> CRLF line feed carriage return
+#string literal / raw
+print (r'C\new folder')
+#multi line literal sesuai yg kita tulis
+#multi line literal dan raw
+print(r"""
+nama saya ucup
+kelas 8
+\newuser""")
