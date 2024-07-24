311b6063142c37ce9b9f119a293ffcd21aad1fe7
irham m s <irhammuhammadsyam@gmail.com>
2024-07-24 18:34:26 +0700

    menambah

---
+++ strwidth.py
@@ -0,0 +1,17 @@
+#str width and aligment
+#data
+data_nama = "Perguso"
+data_umur = 25
+data_tinggi = 191
+data_nosepatu = 49
+#data str standar
+data_str = f"nama =  {data_nama},umur = {data_umur},tinggi = {data_tinggi},no sepatu = {data_nosepatu}"
+print ("\n"+5*"="+"Data standar"+5*"=")
+print (data_str)
+
+#str multiline
+data_str = f"nama =  {data_nama}\numur = {data_umur}\ntinggi = {data_tinggi}\nno sepatu = {data_nosepatu}"
+print ("\n"+5*"="+"Data multiline"+5*"=")
+print (data_str)
+
+
